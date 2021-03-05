98a4a59b692e6bb3a1283e7dc8454664d8914512
DankQuack <55870929+DankQuack@users.noreply.github.com>
2021-03-05 20:00:25 +0530

    Create fizzbuzzInPython.py
    Python solution

---
+++ fizzbuzzInPython.py
@@ -0,0 +1,15 @@
+# Write a program that prints the numbers from 1 to 100 and
+# for multiples of ‘3’ print “Fizz” instead of the number and for the multiples of ‘5’ print “Buzz”.
+
+def fizzbuzz():
+    for i in range(1,101):
+        if i%15 == 0:
+            print('FizzBuzz')
+        elif i%3 == 0:
+            print('Fizz')
+        elif i%5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+fizzbuzz()
